329ec9600360daa8a905e4089af6027a5e5ce4ca
jgeorge37 <george.921@osu.edu>
2020-01-26 13:31:35 -0500

    Added initialize method to player.rb

--- Project 1 OnTrack/lib/player.rb
+++ Project 1 OnTrack/lib/player.rb
@@ -1,8 +1,15 @@
 # Created by Jack Thompson - 1/21/2020
 # Edited by Jack Thompson - 1/22/2020 - added attributes
+# Edited by Jing George - 1/26/2020 - initializes with name and score of 0
 #
 # Player object with name and score
 class Player
   attr_accessor :name
   attr_accessor :score
+
+  def initialize(name)
+    @name = name
+    @score = 0
+  end
+
 end
