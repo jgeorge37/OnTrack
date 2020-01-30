2e9b058f8914ffef0c5198750aa3a806095a766c
Von Mbah <vonchude@gmail.com>
2020-01-29 20:07:34 -0500

    Added ifNoSets and replaceCards methods. Made Check methods private.

--- Project 1 OnTrack/lib/table.rb
+++ Project 1 OnTrack/lib/table.rb
@@ -3,6 +3,9 @@
 # Edited by Jack Thompson - 1/25/2020 - Added initialize and fillTable methods
 # Edited by Jack Hanley - 1/27/20 - Added colorCheck, shapeCheck, numberCheck
 # and shadingCheck methods.  Ediited isSet methods to include these methods
+# Edited by Von Mbah - 
+
+
 # Holds current cards on table (pulls from deck)
 class Table
   attr_accessor :currentCards
@@ -22,6 +25,37 @@ class Table
       puts num
     end
   end
+
+  # Adds in 3 more if the user
+  # sees no possible sets
+  def ifNoSets(deck)
+    puts "Replacing 3 cards."
+    3.times { @currentCards.append(deck.cardList.shift) }
+  end
+
+  # Removes cards that were chosen to be a set and then replace them with new cards if
+  # current card list is less than 12 cards
+  def replaceCards(first, second, third, deck) 
+    @currentCards.delete(first)
+    @currentCards.delete(second)
+    @currentCards.delete(third)
+    fillTable(deck)
+  end
+
+  # Created by Jack Thompson - 1/22/20
+  # Edited by Jack Hanley - 1/27/20
+  # Checks to see if three chosen cards make up a set
+  def isSet(card1,card2,card3)
+    isSet = false
+    # Attribute check. Calls other methods to find if attributes meet criteria
+    if (colorCheck==true && shapeCheck==true && numberCheck==true && shadingCheck==true)
+      isSet = true
+    end
+    isSet
+  end
+
+  private
+
   # Created by Jack Hanley - 1/27/20
   # Checks if colors on cards are either all the same or all different
   def colorCheck(card1,card2,card3)
@@ -63,15 +97,5 @@ class Table
 
 
 
-  # Created by Jack Thompson - 1/22/20
-  # Edited by Jack Hanley - 1/27/20
-  # Checks to see if three chosen cards make up a set
-  def isSet(card1,card2,card3)
-    isSet = false
-    # Attribute check. Calls other methods to find if attributes meet criteria
-    if (colorCheck==true && shapeCheck==true && numberCheck==true && shadingCheck==true)
-      isSet = true
-    end
-    isSet
-  end
+  
 end
