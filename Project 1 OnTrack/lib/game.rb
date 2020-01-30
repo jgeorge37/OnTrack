5ce774f78e3e3ab240cf01203a5a7d57257f7034
Von Mbah <vonchude@gmail.com>
2020-01-29 20:05:23 -0500

    Added in new methods that replace or added new cards

--- Project 1 OnTrack/lib/game.rb
+++ Project 1 OnTrack/lib/game.rb
@@ -1,7 +1,7 @@
 # Created by Jack Thompson - 1/21/2020
 # Edited by Jack Thompson - 1/22/2020 - Laid out foundation of program
 # Edited by Jing George - 1/26/2020 - Incorporation of new PlayerGroup class
-#
+# Edit by Von Mbah - 1/29/2020 - Changed a bit of game logic as well as included replacing cards
 # Controls overall game functionality
 
 require_relative 'deck'
@@ -25,12 +25,13 @@ gameOver = false
 
 until gameOver do
   # Display table
-  tableView.render
+  
   # Verify that there is a set (if not, add more cards if possible)
   correctSet = false
 
   # Prompt for input until correct set is entered
   until correctSet
+    tableView.render
     # Prompt for card 1
     puts("Enter the first card.")
     card1 = gets.chomp
@@ -48,15 +49,20 @@ until gameOver do
     name = gets.chomp
 
     puts("You've entered " + card1 + ", " + card2 + ", " + card3 + ".\n")
+
+    if card1 == "n" || card2 == "n" || card3 =="n"
+      table.ifNoSets(deck)
+    else
     # Check if entered cards are a match
       # If match, continue out of loop and +1 to player Score
         playerGroup.updateScore(name, 1)
         correctSet = true
       # Else, return to prompting and -1 point to player Score
           # playerGroup.updateScore(name, -1)
+    end
   end
   # Replace entered cards
-
+  table.replaceCards(table.currentCards[card1.to_i-1], table.currentCards[card2.to_i-1], table.currentCards[card3.to_i-1], deck)
   # If no more cards/sets, end game
 
 end
