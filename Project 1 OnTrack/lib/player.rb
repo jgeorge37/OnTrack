e25555483df4f9b6c1ed5a6ee5d090c5f9f9e790
jgeorge37 <george.921@osu.edu>
2020-02-02 17:39:20 -0500

    Added @param and @return tags to player.rb functions

--- Project 1 OnTrack/lib/player.rb
+++ Project 1 OnTrack/lib/player.rb
@@ -1,12 +1,16 @@
 # Created by Jack Thompson - 1/21/2020
 # Edited by Jack Thompson - 1/22/2020 - added attributes
 # Edited by Jing George - 1/26/2020 - initializes with name and score of 0
+# Edited by Jing George - 2/2/2020 - added @param and @return tags
 #
 # Player object with name and score
 class Player
   attr_accessor :name
   attr_accessor :score
 
+  # @param [String] name
+  #   a unique identifier to associate players with scores
+  # @return [Self]
   def initialize(name)
     @name = name
     @score = 0
