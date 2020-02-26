a65676e7ffa4f7263d19d021ed8f643f5149afb4
Thrump <vonchude@gmail.com>
2020-02-26 17:11:52 -0500

    possibly working datascrape

--- Project 3 OnTrack/app/controllers/dataScraper.rb
+++ Project 3 OnTrack/app/controllers/dataScraper.rb
@@ -1,8 +1,48 @@
 require 'nokogiri'
 require 'mechanize'
 require 'open-uri'
-
 class Scraper
+  
+  def scrape
+    agent = Mechanize.new
+    page = agent.get("https://web.cse.ohio-state.edu/oportal/schedule_display")
+    form = page.form_with(:dom_id => 'filter');
+    form.field_with(:name => 'strm').options.each do |semester|
+      semester.click
+      semester_page = form.submit.parser
+      course_list = semester_page.css('div.panel')
+      course_list.each do |course| 
+       full_title = course.css('div.panel-heading').text.strip.to_s
+       class_name = ClassName.new(name: full_title)
+       class_name.save
+       rows_0 = course.css('table.table').first.css('tr.group0')
+       rows_1 = course.css('table.table').first.css('tr.group1')
+       rows_0.each do |td| 
+          values = td.css('td')
+          course = Teaching.new(class_number: values[0].text.chomp, component: values[1].text.chomp, location: values[2].text.chomp,
+            times: values[3].text.chomp, instructor: values[4].text.chomp, session: values[5].text.chomp, topic: values[6].text.chomp, 
+            semester: semester.text, class_name_id: class_name.id
+          )
+          course.save
+       end
+
+       rows_1.each do |td| 
+        values = td.css('td')
+        course = Teaching.new(class_number: values[0].text.chomp, component: values[1].text.chomp, location: values[2].text.chomp,
+          times: values[3].text.chomp, instructor: values[4].text.chomp, session: values[5].text.chomp, topic: values[6].text.chomp, 
+          semester: semester.text, class_name_id: class_name.id
+        )
+        course.save
+      end
+
+      end
+    end
+    
+  end
+
+  def  list 
+
+  end 
 
   def initialize
     @agent = Mechanize.new
@@ -59,4 +99,5 @@ class Scraper
 end
 #testing purposes
 test = Scraper.new
-test.table_attributes
+test.scrape
+# test.table_attributes
