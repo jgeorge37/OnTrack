7e44675488444810a5963e86dfaadd0222615eb6
Thrump <vonchude@gmail.com>
2020-02-27 00:55:57 -0500

    changes to data_scraper_controller. Teaching doesn't work still

--- Project 3 OnTrack/app/controllers/data_scraper_controller.rb
+++ Project 3 OnTrack/app/controllers/data_scraper_controller.rb
@@ -1,5 +1,8 @@
 class DataScraperController < ApplicationController
   def scrape
+    current_names = ClassName.all
+    secondary_names = []
+    current_classes = Teaching.all
     agent = Mechanize.new
     page = agent.get("https://web.cse.ohio-state.edu/oportal/schedule_display")
     form = page.form_with(:dom_id => 'filter');
@@ -9,34 +12,37 @@ class DataScraperController < ApplicationController
       course_list = semester_page.css('div.panel')
       course_list.each do |course| 
        full_title = course.css('div.panel-heading').text.strip.to_s
-       class_name = ClassName.new(name: full_title)
-       class_name.save
-       rows_0 = course.css('table.table').first.css('tr.group0')
-       rows_1 = course.css('table.table').first.css('tr.group1')
-       rows_0.each do |td| 
-          values = td.css('td')
-          course = Teaching.new(class_number: values[0].text.chomp, component: values[1].text.chomp, location: values[2].text.chomp,
-            times: values[3].text.chomp, instructor: values[4].text.chomp, session: values[5].text.chomp, topic: values[6].text.chomp, 
-            semester: semester.text, class_name_id: class_name.id
-          )
-          course.save
-       end
+       unless current_names.include?(full_title) || secondary_names.include?(full_title)
+        class_name = ClassName.create(name: full_title)
+        secondary_names.unshift(full_title)
+        rows_0 = course.css('table.table').first.css('tr.group0')
+        rows_1 = course.css('table.table').first.css('tr.group1')
+        rows_0.each do |td| 
+            values = td.css('td')
+            p values[0].text
+            course = Teaching.create(class_number: values[0].text.to_i, component: values[1].text, location: values[2].text,
+              times: values[3].text, instructor: values[4].text, session: values[5].text, topic: values[6].text, 
+              semester: semester.text, class_name_id: class_name.id.to_i)
+        end
 
-       rows_1.each do |td| 
-        values = td.css('td')
-        course = Teaching.new(class_number: values[0].text.chomp, component: values[1].text.chomp, location: values[2].text.chomp,
-          times: values[3].text.chomp, instructor: values[4].text.chomp, session: values[5].text.chomp, topic: values[6].text.chomp, 
-          semester: semester.text, class_name_id: class_name.id
-        )
-        course.save
+        rows_1.each do |td| 
+          values = td.css('td')
+          course = Teaching.create(class_number: values[0].text.to_i, component: values[1].text, location: values[2].text,
+            times: values[3].text, instructor: values[4].text, session: values[5].text, topic: values[6].text, 
+            semester: semester.text, class_name_id: class_name.id.to_i)
+        end
       end
 
       end
     end
+    redirect_to :action => "list"
+    return 
   end
 
   def list
+    @names = ClassName.all
     @course = Teaching.all
+    render 'list'
   end
 end
 
