55ae6d3182477780641dfa22e60c5b86986ee7c3
Von Mbah <vonchude@gmail.com>
2020-03-24 13:12:53 -0400

    Change scoring for computer

--- Project 4 OnTrack/computerPlayer.js
+++ Project 4 OnTrack/computerPlayer.js
@@ -1,31 +1,27 @@
 // Created by Snigdha Tiwari 3/17/20
 
-function computer_moves(){
-    var prob_of_win = Math.random();
-    var cp_array = [];
-    var cp_score = Number(document.getElementById('computer_score').textContent);
-    if(prob_of_win > 0.75){
-        cp_array = give_hint(cardsInGrid);
-    }else{
-        cp_array[0] = cardsInGrid[Math.floor(Math.random()*10)];
-        cp_array[1] = cardsInGrid[Math.floor(Math.random()*10)];
-        cp_array[2] = cardsInGrid[Math.floor(Math.random()*10)];
-    }
-    if(is_set(cp_array)){
-        cp_score = cp_score+1;
-        window.alert("Computer got a set")
-    }else{
-        cp_score=cp_score-1;
-        window.alert("Computer did not get a set")
-    }
-    document.getElementById('computer_score').textContent = cp_score;
+function computer_moves() {
+  var prob_of_win = Math.random();
+  var cp_array = [];
+  var cp_score = Number(document.getElementById('computer_score').textContent);
+  if (prob_of_win > 0.75) {
+    cp_array = give_hint(cardsInGrid);
+  } else {
+    cp_array[0] = cardsInGrid[Math.floor(Math.random() * 10)];
+    cp_array[1] = cardsInGrid[Math.floor(Math.random() * 10)];
+    cp_array[2] = cardsInGrid[Math.floor(Math.random() * 10)];
+  }
+  if (is_set(cp_array)) {
+    cp_score += 3;
+    window.alert('Computer got a set');
+  } else {
+    cp_score -= 1;
+    window.alert('Computer did not get a set');
+  }
+  document.getElementById('computer_score').textContent = cp_score;
 
-
-
-    return cp_array;
+  return cp_array;
 }
-
-
 
 /* Return array of 3 cards the computer choose.
 function computer_moves(correct_set, table_size){
@@ -60,12 +56,12 @@ function computer_moves(correct_set, table_size){
  * @param {Array} a items An array containing the items.
  */
 function shuffle(arr) {
-    var j, x, i;
-    for (i = arr.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
-        x = arr[i];
-        arr[i] = arr[j];
-        arr[j] = x;
-    }
-    return arr;
+  var j, x, i;
+  for (i = arr.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = arr[i];
+    arr[i] = arr[j];
+    arr[j] = x;
+  }
+  return arr;
 }
