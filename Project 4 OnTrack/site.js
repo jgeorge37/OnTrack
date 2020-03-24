ebbeb90efb18d41a952e482aa90bc722a042ae5d
Snigdha <snigdhat7@gmail.com>
2020-03-24 18:20:46 -0400

    Fixed the extra hints

--- Project 4 OnTrack/site.js
+++ Project 4 OnTrack/site.js
@@ -320,7 +320,7 @@ window.onload = function() {
         sp_game_view.appendChild(hint);
         hint.addEventListener('click', function() {
           give_hint(cardsInGrid);
-          give_hint_display(cardsInGrid);
+          create_combos_comp(cardsInGrid);
           player_list.player_list[0].score -= 1;
           player_score.textContent = player_list.player_list[0].score;
         });
