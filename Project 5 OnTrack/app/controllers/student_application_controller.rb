4a82691ae65092b67b18b49d5b7e398d55aa0308
Thrump <vonchude@gmail.com>
2020-04-11 17:16:33 -0400

    added select option for class

--- Project 5 OnTrack/app/controllers/student_application_controller.rb
+++ Project 5 OnTrack/app/controllers/student_application_controller.rb
@@ -5,6 +5,10 @@ class StudentApplicationController < ApplicationController
 
     def new
         @classNames = ClassName.all
+        respond_to do |format|
+            format.html { render :new }
+            format.json { render :json => @classNames.to_json }
+        end
     end
 
     def create 
