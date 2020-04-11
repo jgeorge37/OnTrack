80372eb886991bafb766a540838d98d51787f9f9
Jing George <george.921@osu.edu>
2020-04-11 15:12:59 -0400

    dropdowns for filters for course name and sessions in admin_panel

--- Project 5 OnTrack/app/helpers/admin_panel_helper.rb
+++ Project 5 OnTrack/app/helpers/admin_panel_helper.rb
@@ -1,15 +1,15 @@
 module AdminPanelHelper
 
   def get_filter_opts(map)
-    filters = {sem: ["All semesters"], course: ["All courses"], ses: ["All sessions"]}
+    filters = {sem: ["All semesters"], name: ["All courses"], ses: ["All sessions"]}
     map.each do |k, v|
       filters[:sem].push(k[1])
       # cut off rest of class name beyond "CSE ####(H)"
-      filters[:course].push(v[:name][0, v[:name].index(' ', 4)])
+      filters[:name].push(v[:name][0, v[:name].index(' ', 4)])
       filters[:ses].push(v[:session])
     end
     filters[:sem] = filters[:sem].uniq()
-    filters[:course] = filters[:course].uniq()
+    filters[:name] = filters[:name].uniq()
     filters[:ses] = filters[:ses].uniq()
 
     return filters
