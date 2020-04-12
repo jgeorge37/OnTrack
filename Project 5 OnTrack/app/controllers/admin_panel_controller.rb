0904e80f24d9a6dc40d81589bee9b27d49432f95
Jing George <george.921@osu.edu>
2020-04-11 22:29:34 -0400

    display class info on modify page

--- Project 5 OnTrack/app/controllers/admin_panel_controller.rb
+++ Project 5 OnTrack/app/controllers/admin_panel_controller.rb
@@ -1,16 +1,18 @@
 class AdminPanelController < ApplicationController
-
-# admin_panel/index?course[semester]=SP20&course[name]=All+courses&course[session]=All+sessions
-
   def index
+    set_up_vars
+  end
 
-    @names = ClassName.all
-    @courses = Teaching.all
+  def modify
+    set_up_vars
+  end
 
+  def set_up_vars
+    names = ClassName.all
+    courses = Teaching.all
     # Make map for class number, semester -> all other info
     @map = Hash.new
-    @courses.each do |c|
-
+    courses.each do |c|
       done = false
       # get class number and semester for key
       key = [c.class_number, c.semester]
@@ -18,7 +20,7 @@ class AdminPanelController < ApplicationController
       if !@map.key?(key)
         # some classes have different meeting schedules throughout the week
         # each meeting has location, time, instructor(s)
-        @map[key] = {name: @names.find(c.class_name_id).name, session: c.session, component: c.component, num_g: 0, grader_ids: [], meetings: []}
+        @map[key] = {name: names.find(c.class_name_id).name, session: c.session, component: c.component, attd: true, num_g: 0, grader_ids: [], meetings: []}
       # check if only change is additional instructor for an existing meeting
       else
         @map[key][:meetings].each do |r|  # r is each hash in the meeting array of hashes
@@ -29,7 +31,6 @@ class AdminPanelController < ApplicationController
           end
         end
       end
-
       # if changes are needed, add the meeting
       if !done
         # make meeting as a hash
@@ -39,9 +40,8 @@ class AdminPanelController < ApplicationController
         # add meeting to list of meetings
         @map[key][:meetings].push(meeting)
       end
-
     end # end do-each
+  end  # end set_up_vars
 
-  end  # end index
 
 end
