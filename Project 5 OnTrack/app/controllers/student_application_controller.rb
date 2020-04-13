7d869e0bacb3b83342cb0d09391138e2b27c7565
Thrump <vonchude@gmail.com>
2020-04-13 18:28:36 -0400

    basic adding of time

--- Project 5 OnTrack/app/controllers/student_application_controller.rb
+++ Project 5 OnTrack/app/controllers/student_application_controller.rb
@@ -6,7 +6,10 @@ class StudentApplicationController < ApplicationController
     def new
         @classNames = ClassName.all
         @teachings = Teaching.all
-        response = { :classNames => @classNames, :teachings => @teachings}
+        @descriptions = Description.all
+        @meetings = Meeting.all
+        response = { :classNames => @classNames, :teachings => @teachings,
+             :descriptions => @descriptions, :meetings => @meetings}
         respond_to do |format|
             format.html { render :new }
             format.json { render :json => response }
