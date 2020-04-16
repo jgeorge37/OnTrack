e49efeefc40521449c5075cc8a2c6f34cfa769bd
jackt0310 <jackt0310@gmail.com>
2020-04-16 00:23:24 -0400

    User account functionality - Log out doesn't work yet

--- Project 5 OnTrack/config/routes.rb
+++ Project 5 OnTrack/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'admin_panel/index'
   post 'admin_panel/index'
   get 'admin_panel/modify'
--- Project 5 OnTrack/db/schema.rb
+++ Project 5 OnTrack/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_194354) do
+ActiveRecord::Schema.define(version: 2020_04_16_034848) do
 
   create_table "class_names", force: :cascade do |t|
     t.string "name"
@@ -116,6 +116,13 @@ ActiveRecord::Schema.define(version: 2020_04_15_194354) do
     t.string "teacher_lname"
     t.integer "teacher_lname_num"
     t.string "course"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
