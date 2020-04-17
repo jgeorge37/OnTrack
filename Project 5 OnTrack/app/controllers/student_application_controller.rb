4904cf80455af4cf7f90a12de236cde3261ee465
Thrump <vonchude@gmail.com>
2020-04-16 22:33:14 -0400

    Added some code for create method

--- Project 5 OnTrack/app/controllers/student_application_controller.rb
+++ Project 5 OnTrack/app/controllers/student_application_controller.rb
@@ -1,6 +1,6 @@
 class StudentApplicationController < ApplicationController
     def index
-
+        @grader = Grader.all
     end
 
     def new
@@ -17,5 +17,36 @@ class StudentApplicationController < ApplicationController
     end
 
     def create
+        @grader = Grader.new(name: params[:fname] + " " + params[:lname], last_name_dot: params[:lname_dot], gpa: params[:gpa]);
+        if @grader.save
+            if params.has_key?(:course) then
+                newParams = params.require(:course).permit!
+                newParams[:course] do |k, v|
+                    @className = ClassName.find_by(name: key)
+                    @completedCourse = GraderCompletedCourse.create(grader_id: @grader.id, course_id: @className.id, grade: key[:grade])
+                    key[:time].each do |t|
+                        @graderTimeAvailability = GraderTimeAvailability.create(grader_completed_course_id: @completedCourse.id, time: t)
+                    end
+                end
+            end
+            if params.has_key?(:gradeCourse) then 
+                params[:gradedCourse].each do |c|
+                    @className = ClassName.find_by(name: c)
+                    @gradedPreviousCourse = GraderPreviousGradeCourse.create(grader_id: @grader.id, course_id: @className.id)
+                end
+            end
+            redirect_to "/student_application/index", note: 'Succcessfully submitted application'
+        else
+            render "/student_application/index"
+        end
     end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
 end
