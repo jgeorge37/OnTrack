f544b5adc482624f204c92c48cfd2242d2071a65
Jing George <george.921@osu.edu>
2020-04-19 18:16:18 -0400

    fix syntax error in menu controller

--- Project 5 OnTrack/app/controllers/menu_controller.rb
+++ Project 5 OnTrack/app/controllers/menu_controller.rb
@@ -1,6 +1,5 @@
 class MenuController < ApplicationController
   def index
-    if !ClassName.any?
-      redirect_to controller: 'data_scraper', action: 'scrape'
+    if !ClassName.any? then redirect_to controller: 'data_scraper', action: 'scrape' end
   end
 end
