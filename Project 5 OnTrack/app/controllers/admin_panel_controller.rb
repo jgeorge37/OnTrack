28b9bb630f73741127324f747b767e4be4cd8903
Jing George <george.921@osu.edu>
2020-04-19 18:05:37 -0400

    move get_filter_opts for admin panel from helper to controller

--- Project 5 OnTrack/app/controllers/admin_panel_controller.rb
+++ Project 5 OnTrack/app/controllers/admin_panel_controller.rb
@@ -1,13 +1,14 @@
 class AdminPanelController < ApplicationController
   def index
     @filtered = filter_courses(params)
+    @filter_opts = get_filter_opts(params)
   end
 
   def modify
 
   end
 
-  
+
 
   private
 
@@ -29,6 +30,40 @@ class AdminPanelController < ApplicationController
       end
     end
     return fc
+  end
+
+  def get_filter_opts(par)
+    filters = {
+      sem: ["All semesters"],
+      name: ["All courses"],
+      ses: ["All sessions"],
+      open: [ ["Courses with and without open positions", 0], ["Only courses with open positions", 1] ]
+    }
+
+    filters[:sem] = Course.distinct.pluck(:semester)
+    filters[:name] = Description.distinct.pluck(:name)
+    filters[:name].map! {|n| n[0, n.index(' ', 7)]}
+    filters[:ses] = Description.distinct.pluck(:session)
+
+    filters[:sem].unshift("All semesters")
+    filters[:name].unshift("All courses")
+    filters[:ses].unshift("All sessions")
+
+    # if parameters have been passed, the active filter should be the top option
+    if par[:course]
+      filters[:sem].unshift(par[:course][:semester])
+      filters[:name].unshift(par[:course][:name])
+      filters[:ses].unshift(par[:course][:session])
+      if par[:course][:open].to_i == 1
+        filters[:open].reverse!
+      end
+    end
+
+    filters[:sem] = filters[:sem].uniq
+    filters[:name] = filters[:name].uniq
+    filters[:ses] = filters[:ses].uniq
+
+    return filters
   end
 
 end
