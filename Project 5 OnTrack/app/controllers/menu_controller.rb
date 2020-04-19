a2a3c56c48c771659cded143ed9e42fbbc7a320c
Jing George <george.921@osu.edu>
2020-04-19 18:12:31 -0400

    eliminate code from data_scraper scrape, run seed function instead

--- Project 5 OnTrack/app/controllers/menu_controller.rb
+++ Project 5 OnTrack/app/controllers/menu_controller.rb
@@ -2,8 +2,5 @@ class MenuController < ApplicationController
   def index
     if !ClassName.any?
       redirect_to controller: 'data_scraper', action: 'scrape'
-    elsif !Course.any?
-      redirect_to controller: 'data_scraper', action: 'organize'
-    end
   end
 end
