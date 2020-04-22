aba7c8dcceaae4a8eff5d56bdf7ce156c34cf848
jackt0310 <jackt0310@gmail.com>
2020-04-21 21:43:55 -0400

    Added categories to account and changed view based on category

--- Project 5 OnTrack/db/schema.rb
+++ Project 5 OnTrack/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_22_004433) do
+ActiveRecord::Schema.define(version: 2020_04_22_013438) do
 
   create_table "class_names", force: :cascade do |t|
     t.string "name"
@@ -124,6 +124,7 @@ ActiveRecord::Schema.define(version: 2020_04_22_004433) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.boolean "admin"
+    t.string "category"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
