ac49891ee7df79a2462b52e8448a49edbf4a12b1
jackt0310 <jackt0310@gmail.com>
2020-04-21 20:56:15 -0400

    Basic admin functionality - no protection currently

--- Project 5 OnTrack/db/schema.rb
+++ Project 5 OnTrack/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_194717) do
+ActiveRecord::Schema.define(version: 2020_04_22_004433) do
 
   create_table "class_names", force: :cascade do |t|
     t.string "name"
@@ -123,6 +123,7 @@ ActiveRecord::Schema.define(version: 2020_04_20_194717) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
