929da93c5b82e14575e5f7d66201fcf075ae3e9d
Jing George <george.921@osu.edu>
2020-04-23 00:09:38 -0400

    halfway done with updating configurations

--- Project 5 OnTrack/app/controllers/configuration_controller.rb
+++ Project 5 OnTrack/app/controllers/configuration_controller.rb
@@ -3,7 +3,8 @@ class ConfigurationController < ApplicationController
     @view = false
     @change = false
     @courses = []
-    @properties = {}
+    @properties = []
+    @prop_hash = {}
     @with = "with"
     if params[:grouping]
       if params[:grouping][:action] == "view" then @view = true end
@@ -12,8 +13,8 @@ class ConfigurationController < ApplicationController
 
       check_sem = params[:semester].chomp.match(/^(((AU|SP|SU)\d{2}[,][ ])*)(AU|SP|SU)\d{2}$/)
       check_com = params[:component].chomp.match(/^(((LAB|LEC|REC)[,][ ])*)(LAB|LEC|REC)$/)
-      check_num = params[:c_number].chomp.match(/^    ((\d{4,5}[,][ ])*)\d{4,5}   $/)
-      check_nam = params[:c_name].chomp.match(/^((\d{4}([H]?|[.]\d{2}[H]?)[,][ ])*)\d{4}([H]?|[.]\d{2}[H]?)$/)
+      check_num = params[:class_num].chomp.match(/^((\d{4,5}[,][ ])*)\d{4,5}$/)
+      check_nam = params[:name].chomp.match(/^((\d{4}([H]?|[.]\d{2}[H]?)[,][ ])*)\d{4}([H]?|[.]\d{2}[H]?)$/)
 
       puts params
       if !check_sem && params[:semester].chomp != ""
@@ -24,15 +25,33 @@ class ConfigurationController < ApplicationController
         redirect_to action: 'index', alert: 'Components must be formatted as a 3-letter code,
         separated by a comma and space if multiple.'
         return
-      elsif !check_num && params[:c_number].chomp != ""
+      elsif !check_num && params[:class_num].chomp != ""
         redirect_to action: 'index', alert: 'Invalid class number format.'
         return
-      elsif !check_nam && params[:c_name].chomp != ""
+      elsif !check_nam && params[:name].chomp != ""
         redirect_to action: 'index', alert: 'Invalid number-in-name format.'
         return
       end
       organize_properties(params)
       find_courses(params)
+
+    end
+    update = 0
+    if params[:config] # update the configuration for the found classes
+      params[:courses].each {|c| Course.find(c.id).num_graders = params[:config][:num_g].to_i }
+      update = 1
+    end
+    if params[:attend] == "true"
+      params[:courses].each {|c| Course.find(c.id).attendance = true }
+      update = 1
+    elsif params[:attend] == "false"
+      params[:courses].each {|c| Course.find(c.id).attendance = false }
+      update = 1
+    end
+    if update == 1
+      redirect_to action: 'index', notice: 'Courses successfully updated.'
+
+      return
     end
 
   end # end index
@@ -40,16 +59,34 @@ class ConfigurationController < ApplicationController
   private
 
   def organize_properties(params)
-
-
+    # name is not included since the input name is only part of the recorded name
+    sym_arr = ["semester", "component", "class_num", "name"]
+    params.each do |k, v|
+      if sym_arr.include?(k) && params[k]
+        temp = v.split(", ")
+        if !temp.empty?
+          if k != "name" then @prop_hash[k] = temp end
+          @properties.append(temp)
+        end
+      end
+    end # end each
+    if @prop_hash.key?("class_num")
+      @prop_hash["class_num"].map!(&:to_i)
+    end
   end # end organize_properties
 
   def find_courses(params)
     if params[:grouping][:inclusion] == "meet"
-      @courses = Course.where(@properties)
+      @courses = Course.where(@prop_hash)
+      if !params[:name].empty?
+        @courses = @courses.select {|c| !(c.name.split(" ") & params[:name].split(", ")).empty?}
+      end
+    else
+      @courses = Course.where.not(@prop_hash)
+      if !params[:name].empty?
+        @courses = @courses.select {|c| (c.name.split(" ") & params[:name].split(", ")).empty?}
+      end
     end
-
-
 
   end # end find_courses
 end
